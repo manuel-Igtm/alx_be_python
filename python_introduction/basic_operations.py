ab8be535f212bf4bb44ec5cb3b386e5ec5d79121
Immanuel Njogu <njoguimmanuel8@gmail.com>
2024-09-14 22:24:40 +0300

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+#To practice basic arithmetic operations in Python by performing predefined calculations.
+number1 = 10
+number2 = 5
+# Addition
+result_addition = number1 + number2
+print("Addition of number1 and number2 is result:", result_addition)
+# Subtraction
+result_subtraction = number2 - number1
+print("Subtraction of number2 and number1 is result:", result_subtraction)
+#Product
+result_product = number1 * number2
+print("Product of number1 and number2 is result:", result_product)
