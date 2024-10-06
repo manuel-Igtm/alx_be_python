34c2b7f7238343163b2352bcd08b5216955a4b48
Immanuel Njogu <njoguimmanuel8@gmail.com>
2024-10-06 13:43:13 +0300

    task 0 bank acount

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,26 @@
-#Class Definition
-class BankAccount:
-    def __init__(self, initial_balance=0):
-        self._account_balance = initial_balance 
 
-    def deposit(self,amount):
-        self._account_balance += amount
-        return self._account_balance
-    
-    def withdraw(self,amount):
-        if amount > self._account_balance:
-           return True
+class BankAccount:
+    def __init__(self, account_balance = 0.0):
+        #Initialize the account with an optional initial balance.
+        self.account_balance = account_balance  # Encapsulated attribute
+
+    def deposit(self, amount):
+        #Deposit a specified amount to the account balance.
+        # if amount > 0:
+            self.account_balance = self.account_balance + amount
+        # else:
+        #     print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        #Withdraw a specified amount from the account balance if sufficient funds are available.
+        if 0 < amount <= self.account_balance:
+            self.account_balance = self.account_balance - amount
+            return True
         else:
             return False
-        
+
     def display_balance(self):
-        print(f"Current Balance: ${self._account_balance:.2f}")
+        #Print the current account balance.
+        print(f"Current Balance: ${self.account_balance:.2f}")
+
+
